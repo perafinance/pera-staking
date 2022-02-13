1003254af15892daeff4caf44f90002cb3231fe0
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-02-13 19:28:25 +0300

    some tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers"); 
+const private_key = require("./keys/privatekey.json");
+
+const PRIVATE_KEY = private_key.key;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
